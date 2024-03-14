61024dd38ca188cd704d98079e3c9a627afebffb
Inês Carvalho <ij.carvalho@campus.fct.unl.pt>
2024-03-14 15:40:10 +0000

    deploy persistencia v1

---
+++ src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/RegisterResource.java
@@ -0,0 +1,71 @@
+package pt.unl.fct.di.apdc.firstwebapp.resources;
+
+import java.util.logging.Logger;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreException;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Key;
+import com.google.cloud.datastore.Entity;
+import com.google.gson.Gson;
+
+import pt.unl.fct.di.apdc.firstwebapp.util.AuthToken;
+import pt.unl.fct.di.apdc.firstwebapp.util.LoginData;
+
+
+@Path("/register")
+@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+public class RegisterResource {
+	
+	/**
+	 * Logger Object
+	 */
+	private static final Logger LOG = Logger.getLogger(LoginResource.class.getName());
+	
+	private final Gson g = new Gson();
+	
+	private final Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+	
+	public RegisterResource() {}	// Nothing to be done here
+	
+	
+	@POST
+	@Path("/v1")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response doRegister(LoginData data) {
+		LOG.fine("Attemp to register user: " + data.username);
+		/*
+		if(data.username.equals("jleitao") && data.password.equals("password")) {
+			AuthToken at = new AuthToken(data.username);
+			return Response.ok(g.toJson(at)).build();
+		}
+		return Response.status(Status.FORBIDDEN).entity("Incorrect username or password.").build();
+		*/
+		Key userKey = datastore.newKeyFactory().setKind("Person").newKey(data.username);
+		Entity person = Entity.newBuilder(userKey).set("email", "cd@fct.unl.pt").build();
+		try {
+			datastore.add(person);
+			AuthToken at = new AuthToken(data.username);
+		}
+		catch (DatastoreException e) {
+			if ("ALREADY_EXISTS".equals(e.getReason())) {
+			     // entity.getKey() already exists
+				return Response.status(Status.FORBIDDEN).entity("Username already in use.").build();
+			}
+		}
+		AuthToken at = new AuthToken(data.username);
+		return Response.ok(g.toJson(at)).build();
+	}
+	
+
+}
